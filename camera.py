bb97bbae72cb4f6f72688bb2f1454af3d6b1be48
Pavel Kral <pavel@pavelkral.eu>
2015-12-05 18:05:40 +0100

    added camera.py

---
+++ camera.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+from SimpleCV import Camera
+
+import time
+
+cam = Camera(0, {"width": 1280, "height": 720})
+
+while True:
+
+    filename = "./camera/camera_" + str(int(time.time())) + ".png"
+
+    print("Saving image " + filename)
+
+    cam.getImage().save(filename)
+
+    time.sleep(5)
